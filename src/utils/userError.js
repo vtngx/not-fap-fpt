bf2b51e9d2a0794e5b3ea94e1b3c07f0e25cb661
Viet Nguyen <vieth.nguyen2505@gmail.com>
2021-10-26 23:47:11 +0700

    format master

--- src/utils/userError.js
+++
@@ -1,7 +0,0 @@
-module.exports = class UserError extends Error {
-  constructor(statusCode, message, errObj) {
-    super(message)
-    this.statusCode = statusCode
-    this.errObj = errObj
-  }
-}
